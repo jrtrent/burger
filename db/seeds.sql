3d4f498826cff12f158fd429979b0edef7c8ef8f
jrtrent <jessrtrent@gmail.com>
2018-05-23 00:24:57 -0400

    hw

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+SELECT * FROM burgers_db.burgers;
+INSERT INTO burgers (burger_name, devoured) VALUES ("Monster",0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie",0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avocado",0);
